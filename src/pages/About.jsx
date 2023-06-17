6772976a6bda772b815f507b9b57f8977b0e3274
aybekashirbaev <ashirbaevcoder09@gmail.com>
2023-06-17 10:50:22 +0500

    Creat about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import Img from "../assets/about-img.jpg";
+import '../styles/About.scss';
 const About = () => {
   return (
-    <div>
-      This is a About page!
+    <div className="about">
+      <div className="text-card">
+        <span>About Us</span>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore voluptate adipisci illum quidem magni veritatis reprehenderit iste eius, nihil est praesentium ducimus et quos, placeat repudiandae enim dolor animi?
+        </p>
+        <button className='btn'>Sing Up for Free</button>
+      </div>
+      <img src={Img} alt="" />
     </div>
   );
 };
